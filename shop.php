98b962c87ada46460369f19ba876bd0542c41be6
Zahraa AL-Nemer <zahraaal-nemer@Zahraas-MacBook-Pro.local>
2020-06-24 10:46:40 +0300

    Zahra's Update

--- shop.php
+++ shop.php
@@ -17,7 +17,7 @@ require 'includes/dbh.inc.php';
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <!-- Site Metas -->
-        <title>Beaded evening dress details | ZERI</title>
+        <title>Beaded evening dress details | ZERI with MAC Tech Team</title>
 
         <!-- Style border -->
         <style type="text/css">
