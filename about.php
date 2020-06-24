97f2cf28a299c9a9b4106780c4150d10b8744560
yabdald <67262982+yabdald@users.noreply.github.com>
2020-06-24 09:20:12 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,113 @@
+<?php
+$page_title='About us | ZERI';
+include 'includes/header.php';
+//require 'includes/dbh.inc.php';
+ ?>
+
+<head>
+
+   
+    <title>About us</title>
+</head>
+<html lang="en">
+    
+    <!DOCTYPE html>
+    <html lang="en">
+ 
+    <!-- Start All Title Box -->
+    <div class="all-title-box">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <h2>About ZERI</h2>
+                    <ul class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+                        <li class="breadcrumb-item active">About Us</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End All Title Box -->
+
+    <!-- Start About Page  -->
+    <div class="about-box-main">
+        <div class="container">
+            <div class="row">
+				<div class="col-lg-6">
+                    <div class="banner-frame"> <img class="img-fluid" src="images/about-img.jpg" alt="" />
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <h2 class="noo-sh-title-top">We are <span>ZERI</span></h2>
+                    <p>"More than a place to shop. Both buttons hold the concept of integrating fashion,
+                         design, beauty, art, and more. It includes a variety of elements from local,
+                          Gulf and Arab brands from different designs in amazing settings,
+                         as these designs are updated frequently and with new elements with great care."</p>
+
+                    <br>
+
+                    <h2 class="noo-sh-title-top">Our Goal</h2>
+                    <p>"ZERI aims to maintain the highest standards in terms of customer service and product quality,
+                         and to choose a wide range of exhibits to meet the needs of the target market.
+                         This also helps preserve the institution's image and reputation.
+                        We value our team members and encourage their growth and promotion throughout their careers.
+                         We also work to ensure comprehensive training for members of the sales team as well as members
+                          of the accounts and marketing team. We deal with our clients with a high priority and in order
+                           to better serve them and meet their expectations. We understand their lifestyle with experience
+                            and study the needs of the local market."</p>
+
+                </div>
+            </div>
+            <div class="row my-5">
+                <div class="col-sm-6 col-lg-4">
+                    <div class="service-block-inner">
+                        <h3>Our Designs</h3>
+                        <p>All our designs are designed by the best Arab women designers</p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="service-block-inner">
+                        <h3>We are Professional</h3>
+                        <p>Whether in terms of quality, manufacture, design, product, or shipping, don't worry, you're dealing with the best side. </p>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-lg-4">
+                    <div class="service-block-inner">
+                        <h3>We are Expert</h3>
+                        <p>For years we have been in this field!. </p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+    <!-- End About Page -->
+
+   
+
+    <?php
+include 'includes/footer.php';
+ ?>
+
+    <a href="#" id="back-to-top" title="Back to top" style="display: none;">&uarr;</a>
+
+    <!-- ALL JS FILES -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <!-- ALL PLUGINS -->
+    <script src="js/jquery.superslides.min.js"></script>
+    <script src="js/bootstrap-select.js"></script>
+    <script src="js/inewsticker.js"></script>
+    <script src="js/bootsnav.js."></script>
+    <script src="js/images-loded.min.js"></script>
+    <script src="js/isotope.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/baguetteBox.min.js"></script>
+    <script src="js/form-validator.min.js"></script>
+    <script src="js/contact-form-script.js"></script>
+    <script src="js/custom.js"></script>
+</body>
+
+</html>
